5efae46b38875093314e49254a7f880d7fa9529b
Fatima <64161795+hrh-zara@users.noreply.github.com>
2024-05-05 10:33:26 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
-# exit on error
+
 set -o errexit
 
+pip install --upgrade pip
 pip install -r requirements.txt
 
+
 python manage.py collectstatic --no-input
+
+
 python manage.py migrate
+
+
 gunicorn yourdjangoproject.wsgi:application
--m pip install --upgrade pip
